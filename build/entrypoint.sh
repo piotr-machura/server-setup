4263db08bf7e9830a9b7c9c9b23ef1a7925f5e9c
Piotr Machura <piotr.machura16@gmail.com>
2021-03-05 01:04:17 +0100

    Simplify nginx-certbot image build

--- build/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/usr/sbin/crond -f -d 8 &
-/usr/sbin/nginx -g "daemon off;"
--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
-homedir=$(pwd)
+
+# DEPLOYMENT SCRIPT
+# -----------------
+# Note: This is intended for CentOS/Redhat server, but with some modifications
+# can be utilized on a debian/ubuntu based distribution. Simply change dnf
+# commands to apt counterparts and firewall-cmd configuration to UFW (although
+# Docker is known to disregard firewall wrappers and talk directly to iptables)
+
+read -p "Specify your domain name: "
+domain="$REPLY"
+[[ ! -z "$domain" ]] && [[ "$domain" != "piotr-machura.com" ]] && \
+    find . -exec sed -e -i.bak "s/piotr-machura.com/$domain/g" {} \;
+
 # Enable docker repository, install engine and compose
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /user/bin/docker-compose
 chmod +x /usr/bin/docker-compose
 
-# Create directories for docker volumes
-mkdir --parents $homedir/data/html $homedir/data/letsencrypt
-# Build the nginx-certbot image
-cd $homedir/build
-docker build -t nginx-certbot .
-# Run the containers
-cd $homedir
-docker-compose up --daemonize
-# Obtain letsencrypt certificates
-docker exec -it webserver certbot --nginx
+# Configure the firewall
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --reload
+
+# Create directories for docker volumes and logs
+mkdir --parents ./data/html ./data/letsencrypt
+mkdir --parents ./data/log/letsencrypt
+
+# Build the nginx-certbot image and start the containers
+docker-compose up --build --detach
+
+# Obtain SSL certificates
+docker exec -it webserver certbot --nginx --agree-tos
