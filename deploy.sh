ac7ded725b624c2a5a62a028951959ab351beccb
Piotr Machura <piotr.machura16@gmail.com>
2021-04-03 21:27:25 +0200

    Update deploy.sh for new paths

--- deploy.sh
+++ deploy.sh
@@ -17,18 +17,12 @@ _usage() {
 
     Usage:
         init - install missing software, build the images and start the containers.
-        -u  <username> <password> - adds username@$DOMAIN to mailserver and carddav with given password.
+        -u  - adds username@$DOMAIN to mailserver and carddav with given password.
         -s  - obtain SSL certificates for $DOMAIN, www.$DOMAIN, dav.$DOMAIN, and mail.$DOMAIN using Certbot.
         -m  - safely pass arguments to docker-mailserver's 'setup.sh' script located under ./data/mail.sh.
         -h  - display this message."
     exit 1
 }
-
-# Check if all the tools are avalible
-[[ $EUID -ne 0 ]] && echo "This script must be run as root.\n" && _usage
-[[-z "$(command -v docker &>/dev/null)" ]] && echo "No Docker runtime detected. Install it with ./deploy.sh init" && _usage
-[[-z "$(command -v docker-compose &>/dev/null)" ]] && echo "No docker-compose detected. Install it with ./deploy.sh init" && _usage
-[[ ! -f "./data/mail.sh" ]] && echo "No docker-mailserver setup.sh script found. Install it with ./deploy.sh init" && _usage
 
 case $1 in
     "init")
@@ -58,12 +52,13 @@ case $1 in
         firewall-cmd --permanent --zone=public --add-service=http
         firewall-cmd --permanent --zone=public --add-service=https
         firewall-cmd --reload
+        # TODO: Add email firewall rules here
 
         # Get docker-mailserver's setup tool
-        if [[ ! -f "./data/mail.sh" ]]; then
+        if [[ ! -f "./data/mail/mail.sh" ]]; then
             echo "Getting docker-mailserver's setup.sh..."
             curl https://raw.githubusercontent.com/docker-mailserver/docker-mailserver/v9.0.1/setup.sh > ./data/mail.sh
-            chmod +x ./data/mail.sh
+            chmod +x ./data/mail/mail.sh
         fi
         # TODO: Add mailserver DNS help here
 
@@ -74,7 +69,7 @@ case $1 in
         Initial setup complete. Use
         ./deploy.sh ssl
         to obtain ssl certificates and
-        ./deploy.sh user <username> <password>
+        ./deploy.sh -u
         to add username@$DOMAIN to email and carddav."
         # TODO: Add general DNS help here.
         ;;
@@ -86,7 +81,7 @@ case $1 in
         # Add users to mailserver and radicale
         docker exec -t radicale \
             htpasswd -B -b -c /var/radicale/data/users "$user@$DOMAIN" "$pass"
-        ./data/mail.sh -c mail -p ./config/mailserver email add "$user@$DOMAIN" "$pass"
+        ./data/mail/mail.sh -c mail -p ./data/mailserver/config email add "$user@$DOMAIN" "$pass"
         echo "Added $user@$DOMAIN to email and carddav servers."
         ;;
 
@@ -99,11 +94,15 @@ case $1 in
 
     "-m")
         # Pass arguments to docker-mailserver's setup.sh
-        ./data/mail.sh -c mail -p ./config/mailserver "${@:2}"
+        ./data/mail/mail.sh -c mail -p ./data/mailserver/config "${@:2}"
         ;;
 
-    "-h" | *)
+    "-h" )
         [[ "$#" != "1" ]] && echo "Illegal arguments for $1: ${@:2}"
+        _usage
+        ;;
+    * )
+        echo "Unknown argument: $1"
         _usage
         ;;
 esac
